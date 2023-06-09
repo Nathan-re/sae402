82b917f61170a60167c3f9b1998b21a1b8a81c92
Aihos <hugoleray53@gmail.com>
2023-06-09 08:09:36 +0200

    v3 des pages xm

--- app/src/main/java/fr/rey/dev/sae402/MainActivity.java
+++ app/src/main/java/fr/rey/dev/sae402/MainActivity.java
@@ -2,12 +2,14 @@ package fr.rey.dev.sae402;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import java.lang.reflect.Array;
@@ -15,13 +17,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.choix_des_equipes);
+        Button ButtonPlay = (Button) findViewById(R.id.lancer_partie);
+
+        ButtonPlay.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.i("la partie est lancée", "Hey !");
+
+                new Thread(() -> {
+
+
+                    Intent intent3 = new Intent(getApplicationContext(), PartieClassique.class);
+                    startActivity(intent3);
+                }).start();
+
 
 
 
     }
 
 }
+        ));
+
+    }
+}
+
--- app/src/main/java/fr/rey/dev/sae402/historique.java
+++ app/src/main/java/fr/rey/dev/sae402/historique.java
@@ -1,2 +1,80 @@
-package fr.rey.dev.sae402;public class historique {
+package fr.rey.dev.sae402;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class historique extends AppCompatActivity
+{
+
+
+    private JoueurDAO daoQuery;
+
+
+    private Joueur joeueur;
+    private TextView textView;
+    private Button button;
+    private EditText editText;
+    private String nom;
+    private String prenom;
+    private String score;
+    private String date;
+    private String heure;
+    private String duree;
+    private String gagnant;
+    private String perdant;
+    private String scoreGagnant;
+    private String scorePerdant;
+
+
+
+    public void retour(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
+    public void accessDataBase() {
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.historique_matchs);
+
+        Intent intentHistorique = new Intent(getApplicationContext(), MainActivity.class);
+
+        new Thread(() -> {
+            accessDataBase();
+
+
+
+
+            startActivity(intentHistorique);
+        }).start();
+
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
